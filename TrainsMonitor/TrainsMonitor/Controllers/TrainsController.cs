f891f73b8ddbe9184230cd28745856d0b014618c
Vladimir <voweer@gmail.com>
2016-03-20 22:50:54 +0300

    Initial commit

---
+++ TrainsMonitor/TrainsMonitor/Controllers/TrainsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TrainsMonitor.Controllers
+{
+    public class TrainsController : ApiController
+    {
+        // GET: api/Trains
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World!", "Motherfucker!", "Ahahahah!" };
+        }
+
+        // GET: api/Trains/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Trains
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Trains/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Trains/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TrainsMonitor/TrainsMonitor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrainsMonitor.WebApiApplication" Language="C#" %>
